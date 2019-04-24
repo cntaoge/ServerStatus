ad58ba332b23c81188e04bba6c73a5d7e29ad15d
cntaoge <47247997+cntaoge@users.noreply.github.com>
2019-04-24 15:43:24 +0800

    Update run_ss.sh

--- run_ss.sh
+++ run_ss.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#下行的WEB路径你需要修改成你自己的路径！重要的事情再说三遍。
+#下行的--web-dir=路径你需要修改成你自己的路径！重要的事情再说三遍。
 cd /home/ServerStatus/server;./sergate --config=/home/ServerStatus/server/config.json --web-dir=/www/wwwroot/test.com
